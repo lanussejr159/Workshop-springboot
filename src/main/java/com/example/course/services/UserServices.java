730f39135b2026375ca4de575ab8abe64da7f886
Lanusse Junior <juniorjf2011166@gmail.com>
2023-04-24 15:56:13 -0300

    Aula 318

--- src/main/java/com/example/course/services/UserServices.java
+++ src/main/java/com/example/course/services/UserServices.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
+import com.example.course.services.exceptions.DatabaseException;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -21,7 +24,7 @@ public class UserServices {
         return repository.findAll();
     }
 
-    public User findById(Long id){
+    public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
@@ -30,8 +33,14 @@ public class UserServices {
         return repository.save(obj);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
+        try{
         repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id); 
+        } catch (DataIntegrityViolationException e ) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
