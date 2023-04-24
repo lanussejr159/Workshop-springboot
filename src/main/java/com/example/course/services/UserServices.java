ea4885e695e9bd7dfd2a4048e8bd9b543df15a84
Lanusse Junior <juniorjf2011166@gmail.com>
2023-04-24 13:57:21 -0300

    Aula 315

--- src/main/java/com/example/course/services/UserServices.java
+++ src/main/java/com/example/course/services/UserServices.java
@@ -28,4 +28,8 @@ public class UserServices {
     public User insert(User obj) {
         return repository.save(obj);
     }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
